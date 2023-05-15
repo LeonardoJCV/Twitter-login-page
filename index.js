778ffe142326f6e6495883c3c2d6b37bb6d4b5f4
Leonardo José Clemente Vieira <129434293+LeonardoJCV@users.noreply.github.com>
2023-05-15 17:44:06 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const gg = document.querySelector('#gg')
+const ap = document.querySelector('#ap')
+const av = document.querySelector('#av')
+const sh = document.querySelector('#sh')
+
+gg.addEventListener ('mouseover', function(){
+  document.querySelector('#gg')
+  .classList.add('fade')
+})
+gg.addEventListener ('mouseout', function(){
+  document.querySelector('#gg')
+  .classList.remove('fade')
+})
+
+ap.addEventListener ('mouseover', function(){
+  document.querySelector('#ap')
+  .classList.add('fade')
+})
+ap.addEventListener ('mouseout', function(){
+  document.querySelector('#ap')
+  .classList.remove('fade')
+})
+
+av.addEventListener ('mouseover', function(){
+  document.querySelector('#av')
+  .classList.add('fade')
+})
+av.addEventListener ('mouseout', function(){
+  document.querySelector('#av')
+  .classList.remove('fade')
+})
+
+sh.addEventListener ('mouseover', function(){
+  document.querySelector('#sh')
+  .classList.add('fade')
+})
+sh.addEventListener ('mouseout', function(){
+  document.querySelector('#sh')
+  .classList.remove('fade')
+})
+
+
